a7a8c82e76df04f853c83d7a5ccb00a691226aa0
yashika-asrani <yashiikkaa7@gmail.com>
2023-04-24 08:28:32 +0530

    edited the index.js file to include views

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/books',
+    name: 'books',
+    component: () => import('../views/Books.vue')
+  },
+  {
+    path: '/bookedit/:id',
+    name: 'bookedit',
+    component: () => import('../views/BookEdit.vue')
+  },
+  {
+    path: '/issues',
+    name: 'issue',
+    component: () => import('../views/Issues.vue')
+  },
+  {
+    path: '/authors',
+    name: 'author',
+    component: () => import('../views/Authors.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
 
@@ -27,3 +46,4 @@ const router = new VueRouter({
 })
 
 export default router
+
